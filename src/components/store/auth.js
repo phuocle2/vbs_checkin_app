5bf240f2f9ffb6c13ac9638e87719321e85a233e
phuocle2 <issah.le@gmail.com>
2025-06-21 18:14:50 -0700

    using tailscale vpn

--- src/components/store/auth.js
+++ src/components/store/auth.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AuthContext = React.createContext({
     version: '0.1.0',
     greeting: 'https://pspace.site:5001/greeting',
-    profile: 'https://pspace.site:5001/profile',
+    profile: '100.101.160.39/profile',
     update: 'https://pspace.site:5001/update',
 })
 export default AuthContext;

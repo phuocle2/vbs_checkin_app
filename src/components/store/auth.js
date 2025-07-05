edbe4277c62a9eabd02d28da27e0c49aa774cccd
phuocle2 <issah.le@gmail.com>
2025-07-04 20:13:17 -0500

    release v0.2.0

--- src/components/store/auth.js
+++ src/components/store/auth.js
@@ -3,7 +3,8 @@ import React from 'react';
 const AuthContext = React.createContext({
     version: '0.1.0',
     greeting: 'https://pspace.site:5001/greeting',
-    profile: 'https://pspace.site:5001/profile',
+    profile: 'https://pspace.site:5001/checkin',
     update: 'https://pspace.site:5001/update',
+    list: 'https://pspace.site:5001/list',
 })
 export default AuthContext;

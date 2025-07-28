ce125fcda04914139927599bdc03077048e64305
phuocle2 <issah.le@gmail.com>
2025-07-27 21:19:29 -0500

    revise auth

--- src/components/store/auth.js
+++ src/components/store/auth.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const AuthContext = React.createContext({
     version: '0.1.0',
-    greeting: 'https://pspace.site:5001/greeting',
-    profile: 'https://pspace.site:5001/checkin',
-    update: 'https://pspace.site:5001/update',
-    list: 'https://pspace.site:5001/list',
+    // greeting: 'https://pspace.site:5001/greeting',
+    // profile: 'https://pspace.site:5001/checkin',
+    // update: 'https://pspace.site:5001/update',
+    // list: 'https://pspace.site:5001/list',
+    // greeting: 'http://localhost:5001/greeting',
+    // profile: 'http://localhost:5001/checkin',
+    // update: 'http://localhost:5001/update',
+    // list: 'http://localhost:5001/list',
+    greeting: 'http://server.pspace.site:5001/greeting',
+    profile: 'http://server.pspace.site:5001/checkin',
+    update: 'http://server.pspace.site:5001/update',
+    list: 'http://server.pspace.site:5001/list',
 })
 export default AuthContext;
